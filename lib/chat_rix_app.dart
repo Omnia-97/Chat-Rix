20748f60e39cd45c3d0ac5b206df5067c9870d10
DELL <omnianabil1920@gmail.com>
2024-05-21 22:30:15 +0300

    adding screen util for responsive

--- lib/chat_rix_app.dart
+++ lib/chat_rix_app.dart
@@ -1,17 +1,25 @@
 import 'package:chat_app/modules/create_account/create_account.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatRix extends StatelessWidget {
   const ChatRix({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: CreateAccountPage.routeName,
-      debugShowCheckedModeBanner: false,
-      routes: {
-        CreateAccountPage.routeName: (context) => const CreateAccountPage(),
-      },
-    );
+    return ScreenUtilInit(
+        designSize: const Size(430, 932),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            initialRoute: CreateAccountPage.routeName,
+            debugShowCheckedModeBanner: false,
+            routes: {
+              CreateAccountPage.routeName: (context) =>
+                  const CreateAccountPage(),
+            },
+          );
+        });
   }
 }
